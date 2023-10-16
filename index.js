981e9172382b61f98a2e08e1599a998c52ebac29
M3e <morne.louw@gmail.com>
2023-10-16 15:05:37 +0100

    feat: return json to the browser

--- index.js
+++ index.js
@@ -42,10 +42,11 @@ const server = http.createServer((req, res) => {
         res.end('This is the PRODUCT');
     } else if (pathName === '/api') {
         fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
-            const productData = JSON.parse(data);
-            console.log(productData);
+            res.writeHead(200, {
+                'Content-type': 'application/json'
+            });
+            res.end(data);
         });
-        res.end('API');
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
