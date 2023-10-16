1ec5d9e573094e8afeff8a332e80e45b93f5ee13
M3e <morne.louw@gmail.com>
2023-10-16 14:25:05 +0100

    feat: very basic routing example

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 
 /////////////////////////////////////////
@@ -33,7 +34,15 @@ fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
 // SERVER
 
 const server = http.createServer((req, res) => {
-    res.end('Hello from the server!');
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW');
+    } else if (pathName === '/product') {
+        res.end('This is the PRODUCT');
+    } else {
+        res.end('404 - Page not found');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
